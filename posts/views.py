ddb932c817c3e440d15084f137c3738197d098fe
SichangiFelix <sichangifelix23@gmail.com>
2022-10-19 16:55:46 +0300

    post view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
