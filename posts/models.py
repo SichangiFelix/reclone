26b0c3efec7481e7961d193cba8442b3644df923
SichangiFelix <sichangifelix23@gmail.com>
2022-10-19 17:22:02 +0300

    solved typo error

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 100)
     url = models.URLField()
     poster = models.ForeignKey(User, on_delete = models.CASCADE)
-    created = models.DateTimeField(auto_now_delete = True)
+    created = models.DateTimeField(auto_now_add = True)
 
     class Meta:
         ordering = ['-created']
