9355d26860db37f11a632eb40680c4b1169e8b9d
SichangiFelix <sichangifelix23@gmail.com>
2022-10-20 12:43:14 +0300

    Added missing url and created vote serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,7 +7,13 @@ from .models import Vote
 # Create serializers for each model
 #post
 class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.ReadOnlyField(source = 'poster.username')
+    poster_id = serializers.ReadOnlyField(source = 'poster.id')
     class Meta:
         model = Post
-        fields = ['id', 'title', 'poster', 'created']
+        fields = ['id', 'title', 'url', 'poster','poster_id', 'created']
 
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['id']
