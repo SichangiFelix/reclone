3d573b2acf447a1d4c4bc9e7aebacce92bddbaae
SichangiFelix <sichangifelix23@gmail.com>
2022-10-20 12:43:40 +0300

    view for votes

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
 
-from .serializers import PostSerializer
-from .models import Post
+from .serializers import PostSerializer, VoteSerializer
+from .models import Post, Vote
 
 # Create your views here.
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster = self.request.user)
+
+class VoteCreate(generics.CreateAPIView):  
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk = self.kwargs['pk'])
+        return Vote.objects.filter(voter = user, post = post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
+        serializer.save(voter = self.request.user, post = Post.objects.get(pk = self.kwargs['pk']))
